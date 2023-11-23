c8a551ac67414994098c2db9fb7e60d4f07c4ec5
dipkimyen <113224508+dipkimyen@users.noreply.github.com>
2023-11-24 02:54:42 +0700

    Update diabetes_prediction.py
    delete bootstrap dash

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -1,7 +1,6 @@
 # Import các thư viện và modules cần thiết
 from dash import Dash, Input, Output
 import dash_core_components as dcc
-import dash_bootstrap_components as dbc
 import dash_html_components as html
 import pandas as pd
 import numpy as np
